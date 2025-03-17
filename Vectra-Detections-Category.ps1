3a5c2124a178630f8ed5bb7b53ed0a9d69d64144
Devon Bardoul <dbardoul@live.com>
2025-03-17 14:16:25 -0400

    Add files via upload

---
+++ Vectra-Detections-Category.ps1
@@ -0,0 +1,18 @@
+﻿$outarray = @()
+$url = 'https://<redacted>/api/v2.2/detections?category=command&detection=hidden&page_size=5000'
+$type = 'application/json'
+$props = @{
+    Uri = $url 
+    Headers = @{
+    ContentType = $type
+    Authorization = "Token <redacted>"
+    }
+}
+
+$postResponse = Invoke-RestMethod @props -Method 'GET' -Body '{"detection":"Hidden HTTPS Tunnel","page_size":5000,"query_string":"all"}' -SkipCertificateCheck
+$result = $postResponse.results
+
+foreach ($r in $result) {
+    $outarray += $r
+}
+$outarray | Export-Csv "C:\users\<redacted>\documents\vectra.csv" -NoTypeInformation
---
+++ Vectra-Groups.ps1
@@ -0,0 +1,19 @@
+﻿$outarray = @()
+$url = 'https://<redacted>/api/v2.2/groups/66'
+$type = 'application/json'
+$props = @{
+    Uri = $url 
+    Headers = @{
+    ContentType = $type
+    Authorization = "Token <redacted>"
+    }
+}
+
+$postResponse = Invoke-RestMethod @props -Method 'GET' -SkipCertificateCheck
+$result = $postResponse | Select-Object @{Name="members"; Expression={$_.members}}
+
+foreach ($r in $result) {
+    $outarray += $r
+}
+$outarray = ($outarray.members -join "`n")
+$outarray | Out-File "C:\users\<redacted>\documents\vectraGroups.csv"
